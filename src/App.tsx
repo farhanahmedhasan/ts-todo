3effcc495714436f63f16365e654447b5dc6ceec
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-08-21 10:34:18 +0600

    feature/add-todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import InputField from './components/InputField'
 import './App.css'
+import {useState} from "react";
+import {iTodo} from "./model.ts";
 
 function App() {
+    const [todos, setTodos] = useState<iTodo[]>([])
+
+    function handleAddTodo(todo:iTodo) {
+        setTodos([...todos,todo])
+    }
+
   return (
     <div className="App">
       <span className='heading'>Taskify</span>
-      <InputField />
+      <InputField onAddTodo={handleAddTodo}/>
     </div>
   )
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,25 @@
 import './styles.css'
+import React, {useState} from "react";
+import {iTodo} from "../model.ts";
 
-export default function InputField() {
+interface iProps{
+    onAddTodo :  (todo: iTodo) => void
+}
+
+export default function InputField({onAddTodo}: iProps) {
+    const [todo, setTodo] = useState<string>("")
+    function handleSubmit(e: React.FormEvent<EventTarget>) {
+        e.preventDefault()
+        if (!todo) return
+
+        const newTodo = {id: Date.now(), todo, isCompleted: false}
+        onAddTodo(newTodo)
+        setTodo("")
+    }
+
   return (
-    <form className="input">
-        <input type="text" className="input__box" placeholder="Enter a text"/>
+    <form className="input" onSubmit={handleSubmit}>
+        <input type="text" className="input__box" placeholder="Enter a text" value={todo} onChange={(e)=> setTodo(e.target.value)}/>
         <button className="input__submit" type="submit">Go</button>
     </form>
   )
